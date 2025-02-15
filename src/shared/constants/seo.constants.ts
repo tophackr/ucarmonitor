d436d76afb740ea22461e27ee6ca1c5e797f3925
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-15 14:51:31 +0300

    refactor: add ceo constants

---
+++ src/shared/constants/seo.constants.ts
@@ -0,0 +1,3 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+export const SITE_NAME = process.env.NEXT_PUBLIC_TITLE
+export const SITE_DESCRIPTION = process.env.NEXT_PUBLIC_DESCRIPTION
