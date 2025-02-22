7e46763703fe6e1a91d2de7e16f72018919ccfb7
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-23 00:41:47 +0300

    fix: improve locale fetching logic to handle user messages

--- src/shared/i18n/getLocales.ts
+++ src/shared/i18n/getLocales.ts
@@ -1,22 +1,27 @@
+import deepmerge from 'deepmerge'
 import { defaultLocale, locales } from './config'
 import type { Locale } from './types/Locale'
 import type { TranslationConfig } from './types/Translation'
 
-function isDefaultLocale(locale: Locale): Locale {
-    return locale === defaultLocale || !locales.includes(locale)
-        ? defaultLocale
-        : locale
+function isNotDefaultLocale(locale: Locale): boolean {
+    return locale !== defaultLocale && locales.includes(locale)
 }
 
 export async function getLocales(
     locale: Locale = defaultLocale
 ): Promise<TranslationConfig> {
-    const fetchMessages = await import(
-        `@/shared/i18n/locales/${isDefaultLocale(locale)}.json`
-    )
+    const [defaultMessages, userMessages] = await Promise.all([
+        import('./locales/ru.json').then(m => m.default),
+        isNotDefaultLocale(locale) &&
+            import(`./locales/${locale}.json`).then(m => m.default)
+    ])
+
+    const mergedMessaged = userMessages
+        ? deepmerge(defaultMessages, userMessages)
+        : defaultMessages
 
     return {
         locale,
-        messages: fetchMessages.default
+        messages: mergedMessaged
     }
 }
