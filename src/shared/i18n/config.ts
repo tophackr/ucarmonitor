68d848784279de6afacac0eafea87a4826a6c8d0
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 19:31:39 +0300

    fix: update import path for Locale type and comment out localesMap definition

--- src/shared/i18n/config.ts
+++ src/shared/i18n/config.ts
@@ -1,4 +1,4 @@
-import { Locale } from './types'
+import { Locale } from './types/Locale'
 
 export const defaultLocale = Locale.ru
 
@@ -6,7 +6,7 @@ export const defaultTimeZone = 'Europe/Moscow'
 
 export const locales = [defaultLocale, Locale.en] as const
 
-export const localesMap = [
+/* export const localesMap = [
     { key: Locale.ru, title: 'Russian', subtitle: 'Русский' },
     { key: Locale.en, title: 'English', subtitle: 'English' }
-]
+] */
