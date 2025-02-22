d6c74cc79eb0236b9c3ddc27653dec840f45434d
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 19:22:28 +0300

    feat: add Car interface and related types for car properties management

---
+++ src/entities/cars/types/Car.ts
@@ -0,0 +1,26 @@
+export interface ICar {
+    id: string
+    brand: string
+    model?: string
+    name?: string
+    default?: boolean
+    year?: Date
+    fuel?: CarFuel
+    mileage: number
+    odometerUnits: CarOdometerUnits
+    engineHoursEnabled: boolean
+    engineHours?: number
+}
+
+export enum CarFuel {
+    Gasoline = 'gasoline',
+    Gas = 'gas',
+    Hybrid = 'hybrid',
+    Diesel = 'diesel',
+    Electric = 'electric'
+}
+
+export enum CarOdometerUnits {
+    kilometers = 'kilometers',
+    miles = 'miles'
+}
---
+++ src/entities/cars/types/CarProps.ts
@@ -0,0 +1,13 @@
+import type { ICar } from './Car'
+
+export interface CarProps {
+    car: ICar
+}
+
+export interface CarIdProps {
+    carId: string
+}
+
+export interface CarSlugProps {
+    slug: string[]
+}
