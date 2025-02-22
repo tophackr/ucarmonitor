4d7bb752b41e37a247a7beb39c13ff665262bfbf
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 18:54:02 +0300

    refactor: update tailwind.config.ts to enhance theme colors and expand content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/widgets/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/entities/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/shared/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                base: 'var(--tg-theme-bg-color)',
+
+                secondary: 'var(--tg-theme-secondary-bg-color)',
+                section: 'var(--tg-theme-section-bg-color)',
+                content: 'var(--tg-theme-text-color)',
+
+                accent: 'var(--tg-theme-accent-text-color)',
+                destructive: 'var(--tg-theme-destructive-text-color)',
+                hint: 'var(--tg-theme-hint-color)',
+                subtitle: 'var(--tg-theme-subtitle-text-color)',
+                link: 'var(--tg-theme-link-color)',
+
+                button: 'var(--tg-theme-button-color)',
+                'button-content': 'var(--tg-theme-button-text-color)',
+
+                header: 'var(--tg-theme-header-bg-color)',
+                'header-content': 'var(--tg-theme-section-header-text-color)',
+                bottom: 'var(--tg-bottom-bar-color)'
+            }
+        }
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: []
+} satisfies Config
