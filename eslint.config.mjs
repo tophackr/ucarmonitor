c9a2e384c0c4eb3e8145b844cbfe0edf3a859fe5
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 22:04:19 +0300

    refactor: migrate ESLint configuration from JSON to ES module format

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname
+})
+
+const eslintConfig = [
+    ...compat.extends(
+        '@feature-sliced/eslint-config/rules/layers-slices',
+        'next/core-web-vitals',
+        'next/typescript',
+        'prettier'
+    )
+]
+
+export default eslintConfig
