0f588d641e2d50513be29494b2ec357a6f01e3af
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 22:03:27 +0300

    fix: update StoreProvider import path in Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from 'react'
 import { TelegramProvider } from '@/entities/telegram'
 import { I18nProvider } from '@/shared/i18n'
-import { StoreProvider } from '@/shared/store'
+import { StoreProvider } from './store/provider'
 
 export function Providers({ children }: PropsWithChildren) {
     return (
