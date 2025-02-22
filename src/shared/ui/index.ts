8a62c7a21b2ddc38cec35cd94f7c5d8bfe70c1a7
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 19:46:32 +0300

    feat: create index file to export shared UI components for easier imports

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './buttons'
+export * from './cells'
+export { Link } from './Link'
+export { Loader } from './Loader'
+export * from './lotties'
+export * from './lucide-icon'
+export { Modal } from './Modal'
