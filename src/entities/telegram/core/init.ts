71c6c859e46dc51dd9c0a11a3bb23648ccb3b558
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 19:22:52 +0300

    feat: add initialization function for Telegram app dependencies and components

---
+++ src/entities/telegram/core/init.ts
@@ -0,0 +1,52 @@
+import {
+    $debug,
+    backButton,
+    initData,
+    init as initSDK,
+    mainButton,
+    miniApp,
+    settingsButton,
+    themeParams,
+    viewport
+} from '@telegram-apps/sdk-react'
+
+/**
+ * Initializes the application and configures its dependencies.
+ */
+export function init(debug: boolean): void {
+    // Set @telegram-apps/sdk-react debug mode.
+    $debug.set(debug)
+
+    // Initialize special event handlers for Telegram Desktop, Android, iOS, etc.
+    // Also, configure the package.
+    initSDK()
+
+    // Mount all components used in the project.
+    if (backButton.isSupported()) backButton.mount()
+    if (!mainButton.setParams.isAvailable()) mainButton.mount()
+    if (settingsButton.mount.isAvailable()) settingsButton.mount()
+
+    if (!miniApp.isMounted()) miniApp.mount()
+    if (!themeParams.isMounted()) themeParams.mount()
+
+    initData.restore()
+
+    if (!viewport.isMounted()) {
+        void viewport
+            .mount()
+            .then(() => {
+                if (!viewport.isCssVarsBound()) viewport.bindCssVars()
+            })
+            .catch(e => {
+                console.error('Something went wrong mounting the viewport', e)
+            })
+    }
+
+    if (!miniApp.isCssVarsBound()) miniApp.bindCssVars()
+    if (!themeParams.isCssVarsBound()) themeParams.bindCssVars()
+
+    // Add Eruda if needed.
+    if (debug) {
+        import('eruda').then(lib => lib.default.init()).catch(console.error)
+    }
+}
