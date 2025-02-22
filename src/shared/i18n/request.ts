2d3f1b16aed4d2bd931409f591eff4af2cb272fa
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 19:32:01 +0300

    feat: enhance locale handling in request configuration with default fallback

--- src/shared/i18n/request.ts
+++ src/shared/i18n/request.ts
@@ -1,9 +1,14 @@
-import { getLocale, getRequestConfig } from 'next-intl/server'
-import { getLocales } from './get-locales'
-import type { Locale } from './types'
+import { getRequestConfig } from 'next-intl/server'
+import { getLocales } from './getLocales'
+import { routing } from './routing'
+import type { Locale } from './types/Locale'
 
-export default getRequestConfig(async () => {
-    const locale = await getLocale()
+export default getRequestConfig(async ({ requestLocale }) => {
+    let locale = await requestLocale
+
+    if (!locale || !routing.locales.includes(locale as Locale)) {
+        locale = routing.defaultLocale
+    }
 
     return getLocales(locale as Locale)
 })
