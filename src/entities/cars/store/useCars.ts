92eb1a61887d278e19ef868773ad54880c71ad3e
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 22:03:46 +0300

    fix: replace useActions with useCarsActions in useCars hook

--- src/entities/cars/store/useCars.ts
+++ src/entities/cars/store/useCars.ts
@@ -1,11 +1,12 @@
-import { useActions, useAppSelector } from '@/shared/store'
+import { useAppSelector } from '@/shared/store'
 import type { ICar } from '../types/Car'
 import { setCars as setCarsCloud } from './cars'
 import { selectCars } from './cars.slice'
+import { useCarsActions } from './useCarsActions'
 
 export function useCars() {
     const cars = useAppSelector(selectCars)
-    const { setCars } = useActions()
+    const { setCars } = useCarsActions()
 
     const setCarsWithCloud = (cars: ICar[]) => {
         setCars(cars)
