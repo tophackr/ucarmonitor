2c490c5fc93a9abf9eae16b81acb28b02b45aa68
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-22 18:58:01 +0300

    feat: integrate next-intl plugin and configure remote image patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./src/shared/i18n/request.ts')
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.icons8.com',
+                pathname: '/color/**'
+            }
+        ]
+    }
+}
 
-export default nextConfig;
+export default withNextIntl(nextConfig)
