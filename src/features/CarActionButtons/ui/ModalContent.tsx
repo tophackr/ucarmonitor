62d72a417eed7c4dfc75c65a2474fa181a12201d
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-23 17:43:27 +0300

    feat: integrate useIsAppleClient hook to conditionally style LinkCell in ModalContent

--- src/features/CarActionButtons/ui/ModalContent.tsx
+++ src/features/CarActionButtons/ui/ModalContent.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import clsx from 'clsx'
+import { useIsAppleClient } from '@/shared/hooks'
 import { LinkCell, LucideIcon } from '@/shared/ui'
 import type { ActionContentProps } from '../types/ActionContentProps'
 
@@ -6,12 +10,15 @@ interface ModalContentProps {
 }
 
 export function ModalContent({ content }: ModalContentProps) {
+    const isApple = useIsAppleClient()
+
     return content.map(({ href, icon, bgColor, name }, index) => (
         <LinkCell
             key={index}
             href={href}
             Icon={<LucideIcon name={icon} />}
             bgColor={bgColor}
+            className={clsx(isApple && 'bg-secondary')}
         >
             {name}
         </LinkCell>
