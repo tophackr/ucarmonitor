2ca7a1b4e501f8ce74fb4fb173dc3d316979c017
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-25 23:24:48 +0300

    fix: move middleware for internationalization routing

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import createMiddleware from 'next-intl/middleware'
+import type { NextRequest } from 'next/server'
+import { locales, routing } from '@/shared/i18n'
+
+const handleI18nRouting = createMiddleware(routing)
+
+export default function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl
+
+    const shouldHandle =
+        pathname === '/' ||
+        new RegExp(`^/(${locales.join('|')})(/.*)?$`).test(
+            request.nextUrl.pathname
+        )
+
+    if (!shouldHandle) {
+        return
+    }
+
+    return handleI18nRouting(request)
+}
--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import createMiddleware from 'next-intl/middleware'
-import { routing } from '@/shared/i18n'
-
-export default createMiddleware(routing)
-
-export const config = {
-    matcher: ['/', `/(ru|en)/:path*`]
-}
