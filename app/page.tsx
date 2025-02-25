988b6a91350befd4ff4360cc138c569e4ce01bb4
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-25 23:24:22 +0300

    refactor: remove unused RootPage component and its associated imports

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-import { defaultLocale } from '@/shared/i18n'
-import { pagesRoute } from '@/shared/routes'
-
-export default function RootPage() {
-    redirect(pagesRoute.cars + defaultLocale)
-}
