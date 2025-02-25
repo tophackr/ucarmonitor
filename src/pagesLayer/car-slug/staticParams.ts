7b815709fa0e49613480fd25952128d8ea0a074b
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-25 22:12:38 +0300

    refactor: simplify generateStaticParams function to return a single slug per category

--- src/pagesLayer/car-slug/staticParams.ts
+++ src/pagesLayer/car-slug/staticParams.ts
@@ -1,8 +1,5 @@
 import { CarCategory } from '@/features/CarActionButtons'
 
 export function generateStaticParams() {
-    return Object.values(CarCategory).map(category => [
-        { slug: [category] },
-        { slug: [category, 'new'] }
-    ])
+    return Object.values(CarCategory).map(category => ({ slug: [category] }))
 }
