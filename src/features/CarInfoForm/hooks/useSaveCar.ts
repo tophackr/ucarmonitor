675bb0713b745c4773a778ba9a7d1cff7d3f921d
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-26 22:12:50 +0300

    feat: optimize saveCar hook with useCallback for improved performance

--- src/features/CarInfoForm/hooks/useSaveCar.ts
+++ src/features/CarInfoForm/hooks/useSaveCar.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { v7 as uuid } from 'uuid'
 import { type ICar, useCars } from '@/entities/cars'
 import { useRouter } from '@/shared/i18n'
@@ -12,27 +13,30 @@ export function useSaveCar(): UseSaveCarProps {
     const { cars, setCarsWithCloud } = useCars()
     const router = useRouter()
 
-    const saveCallback = (data: ICar) => {
-        const emptyData = removeEmptyValues(data)
+    const saveCallback = useCallback(
+        (data: ICar) => {
+            const emptyData = removeEmptyValues(data)
 
-        if (!cars.length) {
-            emptyData['default'] = true
-        }
+            if (!cars.length) {
+                emptyData['default'] = true
+            }
 
-        if ('id' in emptyData) {
-            const updatedCars = cars.map(car =>
-                car.id === emptyData.id ? emptyData : car
-            )
+            if ('id' in emptyData) {
+                const updatedCars = cars.map(car =>
+                    car.id === emptyData.id ? emptyData : car
+                )
 
-            setCarsWithCloud(updatedCars)
-        } else {
-            Object.assign(emptyData, { id: uuid() })
+                setCarsWithCloud(updatedCars)
+            } else {
+                Object.assign(emptyData, { id: uuid() })
 
-            setCarsWithCloud([...cars, emptyData])
-        }
+                setCarsWithCloud([...cars, emptyData])
+            }
 
-        router.push(pagesRoute.carId(emptyData.id))
-    }
+            router.push(pagesRoute.carId(emptyData.id))
+        },
+        [cars, router, setCarsWithCloud]
+    )
 
     return { callback: saveCallback }
 }
