b4c12be1e37ee315d83a30d0fe57ec2d8e879a21
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-02-28 23:43:21 +0300

    refactor: update IconCellProps to use BackgroundColor type for bgColor

--- src/shared/ui/cells/icon/IconCellProps.ts
+++ src/shared/ui/cells/icon/IconCellProps.ts
@@ -1,10 +1,10 @@
 import type { LucideProps } from 'lucide-react'
-import type { CSSProperties } from 'react'
-import type { LucideIconName } from '../../lucide-icon'
+import type { BackgroundColor } from '@/shared/types'
+import type { LucideIconName } from '@/shared/ui'
 
 type OmittedLucideIconProps = Omit<LucideProps, 'name'>
 
 export interface IconCellProps extends OmittedLucideIconProps {
     icon: LucideIconName
-    bgColor: CSSProperties['backgroundColor']
+    bgColor: BackgroundColor
 }
