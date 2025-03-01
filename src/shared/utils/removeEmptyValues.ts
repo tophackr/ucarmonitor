f1b484b32922907f8a1990eec66e149ce4f94753
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-01 21:20:59 +0300

    feat: enhance removeEmptyValues function to optionally remove zero values

--- src/shared/utils/removeEmptyValues.ts
+++ src/shared/utils/removeEmptyValues.ts
@@ -1,9 +1,16 @@
 import omitBy from 'lodash.omitby'
 
-export function removeEmptyValues<T extends object>(obj: T): T {
+export function removeEmptyValues<T extends object>(
+    obj: T,
+    removeZero: boolean = false
+): T {
+    const emptyArr = [null, NaN, undefined, '']
+
+    if (removeZero) {
+        emptyArr.push(0)
+    }
+
     return omitBy(obj, value =>
-        [null, NaN, undefined, ''].includes(
-            value as string | number | null | undefined
-        )
+        emptyArr.includes(value as string | number | null | undefined)
     ) as T
 }
