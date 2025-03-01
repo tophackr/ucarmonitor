d0353cfee2e67324525b6e08891b30ce42a4aaeb
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-01 22:51:50 +0300

    feat: add SettingsButton to Providers

--- src/core/providers.tsx
+++ src/core/providers.tsx
@@ -2,14 +2,16 @@ import type { PropsWithChildren } from 'react'
 import { Toaster } from 'sonner'
 import { TelegramProvider } from '@/entities/telegram'
 import { I18nProvider } from '@/shared/i18n'
+import { SettingsButton } from '@/shared/ui'
 import { StoreProvider } from './store/provider'
 
 export function Providers({ children }: PropsWithChildren) {
     return (
         <StoreProvider>
             <TelegramProvider>
-                <Toaster />
+                <SettingsButton />
                 <I18nProvider>{children}</I18nProvider>
+                <Toaster />
             </TelegramProvider>
         </StoreProvider>
     )
