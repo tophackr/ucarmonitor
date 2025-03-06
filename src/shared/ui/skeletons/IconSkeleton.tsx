1be30bedeae552bef6e5418ee54e7a2e8cae7537
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-06 19:22:01 +0300

    fix: update component for not apple client

--- src/shared/ui/skeletons/IconSkeleton.tsx
+++ src/shared/ui/skeletons/IconSkeleton.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import clsx from 'clsx'
 import type { HTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
+import { useIsAppleClient } from '@/shared/hooks'
 import type { BackgroundColor } from '@/shared/types'
 
 interface IconSkeletonProps extends HTMLAttributes<HTMLDivElement> {
@@ -12,10 +15,14 @@ export function IconSkeleton({
     className,
     ...props
 }: IconSkeletonProps) {
+    const isApple = useIsAppleClient()
+
     return (
         <div
-            style={color && { backgroundColor: color }}
-            className={twMerge(clsx('w-8 h-8 rounded-lg', className))}
+            style={isApple && color ? { backgroundColor: color } : undefined}
+            className={twMerge(
+                clsx('w-8 h-8 rounded-lg', className, !isApple && 'bg-subtitle')
+            )}
             {...props}
         />
     )
