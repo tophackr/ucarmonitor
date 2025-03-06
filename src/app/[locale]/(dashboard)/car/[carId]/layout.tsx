9ebe92fdb13618a4f2b0627bc480ce309d316014
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-06 10:16:30 +0300

    refactor: deleted unused properties and components

--- src/app/[locale]/(dashboard)/car/[carId]/layout.tsx
+++ src/app/[locale]/(dashboard)/car/[carId]/layout.tsx
@@ -1,10 +1,1 @@
-import type { PropsWithChildren } from 'react'
-import { CarContextProvider, type CarIdProps } from '@/entities/car'
-import type { ParamsProps } from '@/shared/types'
-
-export default function Layout({
-    params,
-    children
-}: PropsWithChildren<ParamsProps<CarIdProps>>) {
-    return <CarContextProvider params={params}>{children}</CarContextProvider>
-}
+export { CarContextProvider as default } from '@/entities/car'
