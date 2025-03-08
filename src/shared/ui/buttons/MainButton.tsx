513a55c373d604b9b8cf1bfd1deea982e08b405c
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-08 20:21:51 +0300

    refactor: remove isVisible deps

--- src/shared/ui/buttons/MainButton.tsx
+++ src/shared/ui/buttons/MainButton.tsx
@@ -14,13 +14,11 @@ export const MainButton = memo(function MainButton(updates: Partial<State>) {
         mainButton.setParams(params)
 
         return () => {
-            if (mainButton.isVisible()) {
-                mainButton.setParams({
-                    isVisible: false,
-                    isLoaderVisible: false,
-                    isEnabled: true
-                })
-            }
+            mainButton.setParams({
+                isVisible: false,
+                isLoaderVisible: false,
+                isEnabled: true
+            })
         }
     }, [params])
 
