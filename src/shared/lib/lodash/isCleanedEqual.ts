99ff8b4bab933645c90145f0fd838a8d04a9a820
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-03-10 10:01:07 +0300

    feat: add equal when arg is zero

--- src/shared/lib/lodash/isCleanedEqual.ts
+++ src/shared/lib/lodash/isCleanedEqual.ts
@@ -1,9 +1,13 @@
 import isEqual from 'lodash.isequal'
 import { removeEmptyValues } from './removeEmptyValues'
 
-export function isCleanedEqual(before: object, after: object) {
-    const cleanedBefore = removeEmptyValues(before)
-    const cleanedAfter = removeEmptyValues(after)
+export function isCleanedEqual(
+    before: object,
+    after: object,
+    removeZero?: boolean
+) {
+    const cleanedBefore = removeEmptyValues(before, removeZero)
+    const cleanedAfter = removeEmptyValues(after, removeZero)
 
     return isEqual(cleanedBefore, cleanedAfter)
 }
