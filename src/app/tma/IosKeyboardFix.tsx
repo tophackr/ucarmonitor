c83842bf5a78ac129dd7ef66c352952b6029739b
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-04-30 10:10:28 +0300

    refactor: simplify state management in IosKeyboardFix component for improved clarity and performance

--- src/app/tma/IosKeyboardFix.tsx
+++ src/app/tma/IosKeyboardFix.tsx
@@ -5,45 +5,36 @@ import {
 import { type PropsWithChildren, useEffect, useRef, useState } from 'react'
 
 export function IosKeyboardFix({ children }: PropsWithChildren) {
-    const initialHeightRef = useRef<number>(viewportHeight())
-    const initialBottomRef = useRef<number>(viewportSafeAreaInsetBottom())
+    const initialHeight = useRef(viewportHeight())
+    const initialBottom = useRef(viewportSafeAreaInsetBottom())
 
-    const [fixedBottom, setFixedBottom] = useState(initialBottomRef.current)
-    console.log('initial', initialHeightRef.current, initialBottomRef.current)
+    const [currentHeight, setCurrentHeight] = useState(initialHeight.current)
+    const [currentBottom, setCurrentBottom] = useState(initialBottom.current)
 
     useEffect(() => {
         const interval = setInterval(() => {
             const newBottom = viewportSafeAreaInsetBottom()
             const newHeight = viewportHeight()
 
-            // Костыль для вызова useEffect
-            console.log(fixedBottom)
-
             console.log(
-                'bottom',
+                'newBottom',
                 newBottom,
-                initialBottomRef.current,
-                newBottom !== initialBottomRef.current
+                currentBottom,
+                newBottom !== currentBottom
             )
 
-            if (newBottom !== initialBottomRef.current) {
-                initialHeightRef.current = newHeight
-                initialBottomRef.current = newBottom
-                setFixedBottom(fixedBottom)
+            if (newBottom !== currentBottom) {
+                setCurrentBottom(newBottom)
+                setCurrentHeight(newHeight)
             }
         }, 500)
 
         return () => clearInterval(interval)
-    }, [fixedBottom])
+    }, [currentBottom])
 
-    console.log(
-        'fixedHeight',
-        initialHeightRef.current,
-        viewportHeight(),
-        initialHeightRef.current - viewportHeight()
-    )
+    const keyboardOffset = currentHeight - viewportHeight()
 
-    const keyboardOffset = initialHeightRef.current - viewportHeight()
+    console.log('render', keyboardOffset, currentHeight, viewportHeight())
 
     return (
         <div style={{ paddingBottom: keyboardOffset > 0 ? keyboardOffset : 0 }}>
