d2e22518205a328f8917b004b5bc82154409fce5
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-04-30 10:14:00 +0300

    refactor: correct viewport safe area reference in IosKeyboardFix for accurate state management

--- src/app/tma/IosKeyboardFix.tsx
+++ src/app/tma/IosKeyboardFix.tsx
@@ -1,36 +1,36 @@
 import {
     viewportHeight,
-    viewportSafeAreaInsetBottom
+    viewportSafeAreaInsetTop
 } from '@telegram-apps/sdk-react'
 import { type PropsWithChildren, useEffect, useRef, useState } from 'react'
 
 export function IosKeyboardFix({ children }: PropsWithChildren) {
     const initialHeight = useRef(viewportHeight())
-    const initialBottom = useRef(viewportSafeAreaInsetBottom())
+    const initialTop = useRef(viewportSafeAreaInsetTop())
 
     const [currentHeight, setCurrentHeight] = useState(initialHeight.current)
-    const [currentBottom, setCurrentBottom] = useState(initialBottom.current)
+    const [currentTop, setCurrentTop] = useState(initialTop.current)
 
     useEffect(() => {
         const interval = setInterval(() => {
-            const newBottom = viewportSafeAreaInsetBottom()
+            const newBottom = viewportSafeAreaInsetTop()
             const newHeight = viewportHeight()
 
             console.log(
                 'newBottom',
                 newBottom,
-                currentBottom,
-                newBottom !== currentBottom
+                currentTop,
+                newBottom !== currentTop
             )
 
-            if (newBottom !== currentBottom) {
-                setCurrentBottom(newBottom)
+            if (newBottom !== currentTop) {
+                setCurrentTop(newBottom)
                 setCurrentHeight(newHeight)
             }
         }, 500)
 
         return () => clearInterval(interval)
-    }, [currentBottom])
+    }, [currentTop])
 
     const keyboardOffset = currentHeight - viewportHeight()
 
